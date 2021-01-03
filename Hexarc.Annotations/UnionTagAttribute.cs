074811afcd93793a542c291cab6c5dda183d27f9
Max Koverdyaev <shadeglare@gmail.com>
2021-01-03 18:42:50 +0300

    init

---
+++ Hexarc.Annotations/UnionTagAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hexarc.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class UnionTagAttribute : Attribute
+    {
+        public String TagPropertyName { get; }
+
+        public UnionTagAttribute(String tagPropertyName) =>
+            this.TagPropertyName = tagPropertyName;
+    }
+}
